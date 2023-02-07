03939ab2993f0fbaca7b6121685fa646b937918b
jruchoa <jr.uchoa@gmail.com>
2023-02-07 08:12:11 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;
+import java.util.Scanner ;
 
 import model.entities.Reservation;
 
